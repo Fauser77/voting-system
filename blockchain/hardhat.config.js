fb39f12e81089e28e61d39efc860b519523812e3
João Fauser <joaofausersb@gmail.com>
2025-04-26 01:12:35 -0300

    adding dependencies

---
+++ blockchain/hardhat.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  solidity: {
+    version: "0.8.19",
+    settings: {},
+  },
+  paths: {
+    sources: "./contracts",
+  },
+  networks: {
+    ganache: {
+      url: "http://127.0.0.1:8545",
+    },
+  },
+}
